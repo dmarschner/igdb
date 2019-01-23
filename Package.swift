da1f17ad2f8c561a58eec30556796bd5f18e1f3d
Markus Winter <markus@wntr.me>
2019-01-23 15:41:54 +0100

    Split apicalypse into a seperate package

--- Package.swift
+++ Package.swift
@@ -1,20 +1,17 @@
 // swift-tools-version:4.2
 import PackageDescription
 
-// TODO: Allow .includeAll() with expandable values
-
 let package = Package(
     name: "IGDB",
     products: [
-        // 🕹️ An IGDB API vapor service 
         .library(name: "IGDB", targets: ["IGDB"])
     ],
     dependencies: [
-        // 📦 Vapor dependency injection / inversion of control framework.
+        .package(url: "https://github.com/markuswntr/apicalypse.git", .branch("master")),
         .package(url: "https://github.com/vapor/service.git", from: "1.0.0")
     ],
     targets: [
-        .target(name: "IGDB", dependencies: ["Service"]),
+        .target(name: "IGDB", dependencies: ["Service", "Apicalypse"]),
         .testTarget(name: "IGDBTests", dependencies: ["IGDB"])
     ]
 )
--- Sources/IGDB/Apicalypse/Order.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-/// [Sorting](https://api-docs.igdb.com/#sorting)
-///
-/// Sorting is used to order results by a specific field.
-public enum Order: String {
-    case ascending = "asc"
-    case descending = "desc"
-}
